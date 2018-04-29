fcec67c748da3f933ced3ecad5dd4b699ae7a8b2
Evin Tolentino <tolenti4@seattleu.edu>
2018-04-28 18:55:40 -0700

    hw2 files copied

---
+++ hw2data.js
@@ -0,0 +1,12 @@
+{
+  "data": [
+    {
+      "name": "Evin Tolentino",
+      "email": "tolenti4@seattleu.edu"
+    },
+    {
+      "name": "Raquel Horinagu",
+      "email": "horinagr@seattleu.edu"
+    }
+  ]
+}
