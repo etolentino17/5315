3a6645d1194de6376ddd9aef2b3ad115094f3d68
Evin Tolentino <tolenti4@seattleu.edu>
2018-04-28 19:43:49 -0700

    hw2 files copied

--- hw2data.js
+++ hw2data.js
@@ -3,10 +3,182 @@
     {
       "name": "Evin Tolentino",
       "email": "tolenti4@seattleu.edu"
+      "student id": "1234567"
+      "major/program": "Business Analytics"
+      "class standing": "Graduate"
+      "position": "Assistant Area Coordinator"
+      "residence hall": "Chardin Hall"
     },
     {
       "name": "Raquel Horinagu",
       "email": "horinagr@seattleu.edu"
+      "student id": "1028993"
+      "major/program": "Nursing"
+      "class standing": "Junior"
+      "position": "Desk Coordinator"
+      "residence hall": "Chardin Hall"
+    },
+    {
+      "name": "Gretchen Perrera",
+      "email": "perrerg@seattleu.edu"
+      "student id": "8497384"
+      "major/program": "Political Science"
+      "class standing": "Junior"
+      "position": "Desk Coordinator"
+      "residence hall": "Murphy Apartments"
+    },
+    {
+      "name": "Blake Harrington",
+      "email": "harringb@seattleu.edu"
+      "student id": "0296374"
+      "major/program": "Mathematics"
+      "class standing": "Senior"
+      "position": "Desk Assistant"
+      "residence hall": "Murphy Apartments"
+    },
+    {
+      "name": "Arat Cheruvu",
+      "email": "cheruva@seattleu.edu"
+      "student id": "8846396"
+      "major/program": "Communications"
+      "class standing": "Sophomore"
+      "position": "Desk Assistant"
+      "residence hall": "Campion Hall"
+    },
+    {
+      "name": "Gabi See",
+      "email": "seeg@seattleu.edu"
+      "student id": "893710"
+      "major/program": "Sociology"
+      "class standing": "Sophomore"
+      "position": "Desk Assistant"
+      "residence hall": "Bellarmine Hall"
+    },
+    {
+      "name": "Joelle Neddington",
+      "email": "neddingj@seattleu.edu"
+      "student id": "8846673"
+      "major/program": "Communications"
+      "class standing": "Graduate"
+      "position": "Area Coordinator"
+      "residence hall": "Murphy Apartments"
+    },
+    {
+      "name": "Patricia Rebel",
+      "email": "rebelp@seattleu.edu"
+      "student id": "3347558"
+      "major/program": "Criminology"
+      "class standing": "Junior"
+      "position": "Resident Assistant"
+      "residence hall": "Chardin Hall"
+    },
+    {
+      "name": "Phao Than",
+      "email": "thanp@seattleu.edu"
+      "student id": "7846536"
+      "major/program": "Marketing"
+      "class standing": "Sophomore"
+      "position": "Resident Assistant"
+      "residence hall": "Chardin Hall"
+    },
+    {
+      "name": "Gia Respuesta",
+      "email": "respueg@seattleu.edu"
+      "student id": "4562960"
+      "major/program": "English"
+      "class standing": "Junior"
+      "position": "Resident Assistant"
+      "residence hall": "Chardin Hall"
+    },
+    {
+      "name": "Sapphire Pynch",
+      "email": "pynchs@seattleu.edu"
+      "student id": "2209887"
+      "major/program": "Psychology"
+      "class standing": "Sophomore"
+      "position": "Desk Assistant"
+      "residence hall": "Campion Hall"
+    },
+    {
+      "name": "Saide Radley",
+      "email": "radles@seattleu.edu"
+      "student id": "5470986"
+      "major/program": "Business"
+      "class standing": "Senior"
+      "position": "Resident Assistant"
+      "residence hall": "Chardin Hall"
+    },
+    {
+      "name": "Javier Iglesias",
+      "email": "iglesj@seattleu.edu"
+      "student id": "6740911"
+      "major/program": "Psychology"
+      "class standing": "Senior"
+      "position": "Desk Assistant"
+      "residence hall": "Xavier Hall"
+    },
+    {
+      "name": "Casey Mal",
+      "email": "malc@seattleu.edu"
+      "student id": "1234567"
+      "major/program": "Business"
+      "class standing": "Sophomore"
+      "position": "Desk Assistant"
+      "residence hall": "Bellarmine Hall"
+    },
+    {
+      "name": "Hunter Sedaris",
+      "email": "sedarh@seattleu.edu"
+      "student id": "0908876"
+      "major/program": "Computer Science"
+      "class standing": "Sophomore"
+      "position": "HOIDA"
+      "residence hall": "Chardin Hall"
+    },
+    {
+      "name": "Louisa Santos",
+      "email": "santosl@seattleu.edu"
+      "student id": "4509182"
+      "major/program": "Biology"
+      "class standing": "Junior"
+      "position": "Desk Assistant"
+      "residence hall": "Chardin Hall"
+    },
+    {
+      "name": "Sally Wasowzki",
+      "email": "wasows@seattleu.edu"
+      "student id": "8709432"
+      "major/program": "Finance"
+      "class standing": "Sophomore"
+      "position": "Desk Assistant"
+      "residence hall": "Murphy Apartments"
+    },
+    {
+      "name": "Brant Books",
+      "email": "booksb@seattleu.edu"
+      "student id": "7410267"
+      "major/program": "Economics"
+      "class standing": "Graduate"
+      "position": "Assistant Director"
+      "residence hall": "Campion Hall"
+    },
+    {
+      "name": "Darla O'Brien",
+      "email": "obriend@seattleu.edu"
+      "student id": "0019856"
+      "major/program": "Accounting"
+      "class standing": "Graduate"
+      "position": "HOIDA"
+      "residence hall": "Campion Hall"
+    },
+    {
+      "name": "Rainee Sheffield",
+      "email": "sheffier@seattleu.edu"
+      "student id": "6983203"
+      "major/program": "Nursing"
+      "class standing": "Senior"
+      "position": "Desk Coordinator"
+      "residence hall": "Campion Hall"
     }
   ]
 }
